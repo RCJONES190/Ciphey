1573a4b0b8b935ebc42da549703f62f64b0ca37c
rodney <rcjones190@gmail.com>
2023-08-27 08:52:40 -0500

    Update freq_analysis.py

--- tools/freq_analysis.py
+++ tools/freq_analysis.py
@@ -7,3 +7,54 @@ data = sys.stdin.read()
 analysis = cipheycore.analyse_string(data)
 
 print(json.dumps({i: j / len(data) for i, j in analysis.freqs.items()}))
+ @registry.register
+class Atbash(Decoder[str, str]): @registry.register
+class Atbash(Decoder[str, str]):
+class Atbash(Decoder[str]):
+    def decode(self, ctext: T) -> Optional[U]:
+        """
+        Takes an encoded string and attempts to decode it according to the Atbash cipher. @registry.register
+class Atbash(Decoder[str, str]):
+class Atbash(Decoder[str]):
+    def decode(self, ctext: T) -> Optional[U]:
+        """
+        Takes an encoded string and attempts to decode it according to the Atbash cipher.
+class Atbash(Decoder[str]):
+    def decode(self, ctext: T) -> Optional[U]:
+        PGP KeyID: 6C8E212E75CDBD79
+PGP Key Fingerprint: E620 1F11 D3AC 6FEC 4CC5 8CA6 6C8E 212E
+75CD BD79
+
+Thank you for posting this Mark. I have been working on the Coram Nobis
+issue myself lately, and it's a topic that those who've been down the road to
+prison might want to research.
+
+On Fri, Aug 22, 2008 at 10:55 AM, sheisaceo <sheisaceo@yahoo. com>
+wrote:
+
+I wasn't aware that we "flame" here as I was hoping that this remained
+
+an ego-less forum.
+
+Thank you for this. I was busy with writs of habeas corpus and the
+crooks just keep finding ways to try to slime and weasel out of
+documents by refusal to file and even overt theft of the documents.
+You would not believe some of the antics of these people. They HAVE
+TO KNOW WHAT THEY ARE DOING IS ILLEGAL WITH WHAT
+THEY DO.
+
+This is from their Yahoo Group:
+
+Please visit
+
+hitp://tinyurl.com/6h8a3u
+
+& listen to the following 3 conference calls by Rod Class for an excellent
+presentation of how this strategy developed:
+
+You want to listen to:
+1.) June 20 EPISODE 71 - FOREIGN RELATION & FOREIGN STATE
+
+2.) July 11 EPISODE 75 - FSIA PART 3 CORPORATION
+3.) July 19 EPISODE 78 - FOUNDATION OF THE FSIA
+
